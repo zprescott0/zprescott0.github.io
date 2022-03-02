b42412c92a984483162599f9d766d4ac183f7253
zprescott0 <76888154+zprescott0@users.noreply.github.com>
2022-03-02 08:00:43 -0500

    Set up on GitHub Pages

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let cupcakeProject = document.getElementById('cupcake-project');
+let teaProject = document.getElementById('tea-project');
+
+//This function displays or hides information about a given project upon being clicked.
+function displayData () {
+    let hiddenContent = this.getElementsByClassName('description');
+
+    if (hiddenContent[0].style.display == 'none' || hiddenContent[0].style.display == '')
+    {
+        hiddenContent[0].style.display = 'inline-block';
+        this.style.width = '40%';
+    }
+    else
+    {
+        hiddenContent[0].style.display = 'none';
+        this.style.width = '20%';
+    }
+}
+
+//Obtain projects from website
+let projects = document.getElementsByClassName('project');
+
+//Add event listeners
+cupcakeProject.addEventListener('click', displayData);
+teaProject.addEventListener('click', displayData);
